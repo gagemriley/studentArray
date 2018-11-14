00a9a1d66b5912fcb555098246833954b69d4845
Gage Riley <gagemriley@gmail.com>
2018-11-13 20:06:22 -0500

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace studentArray
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.DescriptionLbl = new System.Windows.Forms.Label();
+            this.StudentLbl = new System.Windows.Forms.Label();
+            this.GradeLbl = new System.Windows.Forms.Label();
+            this.AddStudentBtn = new System.Windows.Forms.Button();
+            this.DisplayArrayBtn = new System.Windows.Forms.Button();
+            this.ResetBtn = new System.Windows.Forms.Button();
+            this.StudentBox = new System.Windows.Forms.TextBox();
+            this.GradeBox = new System.Windows.Forms.TextBox();
+            this.ShowArrayLbl = new System.Windows.Forms.Label();
+            this.LineLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // DescriptionLbl
+            // 
+            this.DescriptionLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.DescriptionLbl.Location = new System.Drawing.Point(40, 29);
+            this.DescriptionLbl.Name = "DescriptionLbl";
+            this.DescriptionLbl.Size = new System.Drawing.Size(540, 100);
+            this.DescriptionLbl.TabIndex = 0;
+            this.DescriptionLbl.Text = resources.GetString("DescriptionLbl.Text");
+            // 
+            // StudentLbl
+            // 
+            this.StudentLbl.AutoSize = true;
+            this.StudentLbl.Location = new System.Drawing.Point(84, 148);
+            this.StudentLbl.Name = "StudentLbl";
+            this.StudentLbl.Size = new System.Drawing.Size(75, 13);
+            this.StudentLbl.TabIndex = 1;
+            this.StudentLbl.Text = "Student Name";
+            // 
+            // GradeLbl
+            // 
+            this.GradeLbl.AutoSize = true;
+            this.GradeLbl.Location = new System.Drawing.Point(325, 148);
+            this.GradeLbl.Name = "GradeLbl";
+            this.GradeLbl.Size = new System.Drawing.Size(210, 13);
+            this.GradeLbl.TabIndex = 2;
+            this.GradeLbl.Text = "Course Grade as a Number (e.q. 99 is 99%)";
+            // 
+            // AddStudentBtn
+            // 
+            this.AddStudentBtn.Location = new System.Drawing.Point(157, 214);
+            this.AddStudentBtn.Name = "AddStudentBtn";
+            this.AddStudentBtn.Size = new System.Drawing.Size(311, 47);
+            this.AddStudentBtn.TabIndex = 3;
+            this.AddStudentBtn.Text = "Add Student to Grading Array";
+            this.AddStudentBtn.UseVisualStyleBackColor = true;
+            this.AddStudentBtn.Click += new System.EventHandler(this.AddStudentBtn_Click);
+            // 
+            // DisplayArrayBtn
+            // 
+            this.DisplayArrayBtn.Location = new System.Drawing.Point(87, 302);
+            this.DisplayArrayBtn.Name = "DisplayArrayBtn";
+            this.DisplayArrayBtn.Size = new System.Drawing.Size(448, 29);
+            this.DisplayArrayBtn.TabIndex = 4;
+            this.DisplayArrayBtn.Text = "Display Array - Display Each Student\'s Info from the Stored Array AND Calculate A" +
+    "verage";
+            this.DisplayArrayBtn.UseVisualStyleBackColor = true;
+            this.DisplayArrayBtn.Click += new System.EventHandler(this.DisplayArrayBtn_Click);
+            // 
+            // ResetBtn
+            // 
+            this.ResetBtn.Location = new System.Drawing.Point(63, 494);
+            this.ResetBtn.Name = "ResetBtn";
+            this.ResetBtn.Size = new System.Drawing.Size(494, 30);
+            this.ResetBtn.TabIndex = 5;
+            this.ResetBtn.Text = "Clear Text Box and Clear Away and Reset Student Counter/Array Index";
+            this.ResetBtn.UseVisualStyleBackColor = true;
+            this.ResetBtn.Click += new System.EventHandler(this.ResetBtn_Click);
+            // 
+            // StudentBox
+            // 
+            this.StudentBox.Location = new System.Drawing.Point(87, 164);
+            this.StudentBox.Name = "StudentBox";
+            this.StudentBox.Size = new System.Drawing.Size(124, 20);
+            this.StudentBox.TabIndex = 6;
+            // 
+            // GradeBox
+            // 
+            this.GradeBox.Location = new System.Drawing.Point(328, 164);
+            this.GradeBox.Name = "GradeBox";
+            this.GradeBox.Size = new System.Drawing.Size(207, 20);
+            this.GradeBox.TabIndex = 7;
+            // 
+            // ShowArrayLbl
+            // 
+            this.ShowArrayLbl.BackColor = System.Drawing.Color.PowderBlue;
+            this.ShowArrayLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ShowArrayLbl.Location = new System.Drawing.Point(40, 350);
+            this.ShowArrayLbl.Name = "ShowArrayLbl";
+            this.ShowArrayLbl.Size = new System.Drawing.Size(540, 129);
+            this.ShowArrayLbl.TabIndex = 8;
+            // 
+            // LineLbl
+            // 
+            this.LineLbl.Location = new System.Drawing.Point(84, 270);
+            this.LineLbl.Name = "LineLbl";
+            this.LineLbl.Size = new System.Drawing.Size(451, 11);
+            this.LineLbl.TabIndex = 29;
+            this.LineLbl.Text = "---------------------------------------------------------------------------------" +
+    "--------------------------------------------------------------------------------" +
+    "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 555);
+            this.Controls.Add(this.LineLbl);
+            this.Controls.Add(this.ShowArrayLbl);
+            this.Controls.Add(this.GradeBox);
+            this.Controls.Add(this.StudentBox);
+            this.Controls.Add(this.ResetBtn);
+            this.Controls.Add(this.DisplayArrayBtn);
+            this.Controls.Add(this.AddStudentBtn);
+            this.Controls.Add(this.GradeLbl);
+            this.Controls.Add(this.StudentLbl);
+            this.Controls.Add(this.DescriptionLbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label DescriptionLbl;
+        private System.Windows.Forms.Label StudentLbl;
+        private System.Windows.Forms.Label GradeLbl;
+        private System.Windows.Forms.Button AddStudentBtn;
+        private System.Windows.Forms.Button DisplayArrayBtn;
+        private System.Windows.Forms.Button ResetBtn;
+        private System.Windows.Forms.TextBox StudentBox;
+        private System.Windows.Forms.TextBox GradeBox;
+        private System.Windows.Forms.Label ShowArrayLbl;
+        private System.Windows.Forms.Label LineLbl;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,151 @@
+﻿//Author: Gage Riley
+//ID: 598483
+//Date: 12/5/2017
+//Goal-Purpose of the Program: This program allows the entry of one or more student’s names and course numeric grades; that also displays the information from the array in a Label on the form.
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace studentArray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //ARRAYS
+        //new array variables to hold the info for each player entered
+
+        const int CONST_array_max_size = 5;
+
+        string[] myArray_StudentNameStr = new string[5];
+        int[] myArray_StudentGradeInt = new int[5];
+
+        int student_array = 0;
+
+
+        //add student info to array button/method
+        private void AddStudentBtn_Click(object sender, EventArgs e)
+        {
+
+            bool myTryParseResultBool;
+
+            string studentName = "";
+            int studentGrade = 0;
+
+            //INPUT-VALIDATION
+            if (StudentBox.Text == "")
+            {
+                MessageBox.Show("Error 101: Invalid Name");
+                return;
+            }
+            studentName = StudentBox.Text;
+
+            myTryParseResultBool = int.TryParse(GradeBox.Text, out studentGrade);
+            if (myTryParseResultBool == false || studentGrade > 100 || studentGrade < 0)
+            {
+                MessageBox.Show("Error 201: Invalid Grade");
+                return;
+            }
+
+            //ARRAYS: adding students to array
+            if (student_array >= CONST_array_max_size)
+            {
+                MessageBox.Show("Array is full, cannot add any more to the Array...");
+            }
+            else
+            {
+                myArray_StudentNameStr[student_array] = studentName;
+                myArray_StudentGradeInt[student_array] = studentGrade;
+
+                MessageBox.Show("OK, added the student to the Array at Array index" + "[" + student_array + "]");
+                StudentBox.Text = "";
+                GradeBox.Text = "";
+                StudentBox.Focus();
+            }
+
+            //OUTPUT
+            student_array = student_array + 1;
+        }
+
+        //display the array in a label
+        private void DisplayArrayBtn_Click(object sender, EventArgs e)
+        {
+
+            string myString = "";
+            decimal myTotalGrades = 0;
+            decimal myCount = 0;
+
+            myString = "STUDENT DATA ENTERED SO FAR:\n";
+
+            //loop through array_index
+            for (int i = 0; i < myArray_StudentNameStr.Length; i++)
+            {
+
+                myString = myString +
+                    "Array[" + i + "]  ==>  " +
+                    "Student #" + (i + 1);
+
+                if (myArray_StudentGradeInt[i] == 0)
+                {
+                    myString = myString + "    (NOT ENTERED)";
+                }
+                else
+                {
+                    myString = myString +
+                    "    Name:" + myArray_StudentNameStr[i] +
+                        "    Grade:" + myArray_StudentGradeInt[i];
+
+                    myCount++;
+                    myTotalGrades = myTotalGrades + myArray_StudentGradeInt[i];
+
+                }
+
+                myString = myString + "\n";
+            }
+
+
+            if (myCount > 0)
+            {
+                myString = myString +
+                    "TOTAL STUDENTS:" + myCount +
+                    "    AVERAGE GRADE:" + (myTotalGrades / myCount) +
+                    "\n";
+
+            }
+
+
+            //OUTPUT
+            ShowArrayLbl.Text = myString;
+
+        }
+
+        //reset the form
+        private void ResetBtn_Click(object sender, EventArgs e)
+        {
+
+            StudentBox.Text = "";
+            GradeBox.Text = "";
+            ShowArrayLbl.Text = "";
+
+            StudentBox.Focus();
+
+            //ARRAYS
+            //clear out the array variables
+            student_array = 0;
+            myArray_StudentNameStr = new string[CONST_array_max_size];
+            myArray_StudentGradeInt = new int[CONST_array_max_size];
+        }
+    }
+}
